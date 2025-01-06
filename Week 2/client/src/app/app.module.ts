0d94f0a3d986cb3313a6ea797802a4cd3b25a330
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2025-01-06 12:27:47 +0530

    refactor: delete unused packages

--- Week 2/client/src/app/app.module.ts
+++ Week 2/client/src/app/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { LoginComponent } from './pages/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NavbarComponent } from './include/navbar/navbar.component';
 import { FooterComponent } from './include/footer/footer.component';
-import { ProfileComponent } from './pages/profile/profile.component';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgToastModule } from 'ng-angular-popup';
